10f06af67005ce84e0b6709d96ce2dedd3ca371f
burningwolf <keksonadze@astralinux.localdomain>
2025-06-12 00:22:15 +0300

    fortnite

---
+++ src/main.cpp
@@ -0,0 +1,140 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <string>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+enum KeyPressSurfaces{
+    KEY_PRESS_SURFACE_DEFAULT,
+    KEY_PRESS_SURFACE_UP,
+    KEY_PRESS_SURFACE_DOWN,
+    KEY_PRESS_SURFACE_LEFT,
+    KEY_PRESS_SURFACE_RIGHT,
+    KEY_PRESS_SURFACE_TOTAL
+};
+
+bool init();
+
+bool loadMedia();
+
+void close();
+
+SDL_Window* gWindow = NULL;
+
+SDL_Surface* gScreenSurface = NULL;
+
+SDL_Surface* loadSurface(std::string path);
+
+SDL_Surface* gKeyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
+
+SDL_Surface* gCurrentSurface = NULL;
+
+SDL_Surface* loadSurface(std::string path){
+    SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
+    if(loadedSurface == NULL)
+    {
+        printf("Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+    }
+    return loadedSurface;
+}
+
+bool init(){
+    bool success = true;
+    if(SDL_Init(SDL_INIT_VIDEO)<0){
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    }
+    else{
+        gWindow = SDL_CreateWindow("Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if(gWindow == NULL){
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            success = false;
+        }
+        else{
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
+        }
+    }
+    return success;
+}
+bool loadMedia()
+{
+    bool success = true;
+    gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] = loadSurface("default.bmp");
+    if(gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] == NULL){
+        printf("Failed to load default image!\n");
+        success = false;
+    }
+    gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] = loadSurface("up.bmp");
+    if(gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] == NULL){
+        printf("Failed to load default image!\n");
+        success = false;
+    }
+    gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] = loadSurface("up.bmp");
+    if(gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] == NULL){
+        printf("Failed to load default image!\n");
+        success = false;
+    }
+    return success;
+}
+
+void close()
+{
+    for(int i = 0; i < KEY_PRESS_SURFACE_TOTAL; ++i){
+        SDL_FreeSurface(gKeyPressSurfaces[i]);
+        gKeyPressSurfaces[i] = NULL;
+    }
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+    SDL_Quit();
+}
+
+int main(int argc, char* args[]){
+    if( !init() )
+    {
+        printf( "Failed to initialize!\n" );
+    }
+    else
+    {
+        if( !loadMedia() )
+        {
+            printf( "Failed to load media!\n" );
+        }
+        else
+        {
+            bool quit = false;
+            SDL_Event e;
+            gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+            while(!quit){
+                while(SDL_PollEvent(&e) != 0){
+                    if(e.type == SDL_QUIT){
+                        quit = true;
+                    }
+                    else if(e.type == SDL_KEYDOWN){
+                        switch(e.key.keysym.sym){
+                            case SDLK_w:
+                            gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_UP];
+                            break;
+
+                            case SDLK_s:
+                            gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN];
+                            break;
+                            
+                            default:
+                            gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+                            break;
+                        }
+                    }
+                }
+            //Apply the image
+            SDL_BlitSurface(gCurrentSurface, NULL, gScreenSurface, NULL );
+            //Update the surface
+            SDL_UpdateWindowSurface( gWindow );
+              //Hack to get window to stay up
+            }
+        }
+    }
+        //Free resources and close SDL
+        close();
+        return 0;
+}
